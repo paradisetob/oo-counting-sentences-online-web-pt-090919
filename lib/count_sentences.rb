0a283ec1055cc56e15c224b4644656af9ea57858
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 11:42:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+  "Hi, my name is Sophie.".sentence?
   end
 
   def question?
-
+"What's your name?".question?
   end
 
   def exclamation?
-
+"Hi, my name is Sophie!".exclamation?
   end
 
   def count_sentences
-
+one. two. three?".count_sentences
   end
 end
